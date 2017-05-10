a2a701a4899be6c88b1f838a1a57b3ae93b22563
PhuongPD <PhuongPD@fsoft.com.vn>
2017-05-10 11:04:28 +0700

    Creat main file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+		<link rel="stylesheet" type="text/css"  href="main.css" />
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	</head>
+	<body>
+		<div id="div-body">
+			
+		</div>
+		
+	</body>
+</html>
